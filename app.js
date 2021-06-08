5f3f50a6e3e7ba2268de9d37ba40a763f9a1c32f
Faizan Siddiqui <siddiquimdfaizan82@gmail.com>
2021-06-08 17:37:11 +0500

    done

---
+++ app.js
@@ -0,0 +1,47 @@
+var userArr = [];
+
+function getData() {
+    var userInput = document.getElementById("userInput").value;
+    userArr.push(userInput);
+    var userInput = document.getElementById("userInput").value = "";
+
+    showData();
+    return false;
+}
+
+function showData() {
+    document.getElementById('showData').innerHTML = null;
+    userArr.forEach((value, index) => {
+        var html = `
+        <div class="show col-md-8">
+        <div class="innerDiv">
+            <div>
+                <p class="pra">${value}</p>
+            </div>
+            <div>
+            <div>
+            <button onclick="edit(${index})"><i class="fas fa-pen-square fa-2x"></i></button>
+            </div>
+            <div>
+            <button onclick="dlt(${index})"><i class="fas fa-trash-alt fa-2x"></i></button>
+            </div>
+            </div>
+            </div>
+        </div>
+        `
+        document.getElementById('showData').innerHTML += html
+        console.log(value)
+    })
+}
+
+function dlt(i) {
+    userArr.splice(i, 1);
+    showData();
+}
+
+function edit(i) {
+    userArr[i]
+    var promt = prompt("hello", userArr[i]);
+    userArr.splice(i, 1, promt);
+    showData()
+}
